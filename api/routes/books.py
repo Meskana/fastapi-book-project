7b87d0caa1c4fc9b2f1ba424b00f642348a5756c
marcelinus <marcelinusilonuba21@gmail.com>
2025-02-13 15:03:38 +0100

    fix: books id

--- api/routes/books.py
+++ api/routes/books.py
@@ -68,8 +68,9 @@ async def delete_book(book_id: int) -> None:
 
 
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
-async def get_book(book_id: int):
+async def get_book(book_id: int) -> Book:
     book = db.get_book(book_id)
     if not book:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="book not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book not found")
     return book
+
